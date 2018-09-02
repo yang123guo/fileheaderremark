f1e6c324bc4a018a12f23040f43d4c4dac06e27f
yanggqm <289656703@qq.com>
2018-09-02 23:36:09 +0800

    :art: commit a extension of vscode

--- src/HeaderRemark.js
+++ src/HeaderRemark.js
@@ -19,9 +19,21 @@ class HeaderRemark {
 
     // 执行入口
     init() {
+        // 参考：https://code.visualstudio.com/docs/extensionAPI/vscode-api
         const {
-            
+            workspace, // 当前工作区的 属性 和 方法 和 钩子函数
+            commands,
+            editor
         } =  this.vscode;
+
+        const {
+            getConfiguration, // 获取配置项  getConfiguration(section?: string, resource?: Uri | null)
+            onDidSaveTextDocument, // 当执行保存时触发的钩子函数 onDidSaveTextDocument: Event<TextDocument>
+        } = workspace;
+
+        const {
+            registerCommand, // 注册指令
+        } = commands
     }
 }
 
