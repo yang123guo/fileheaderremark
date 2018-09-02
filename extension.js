f01951d3503f3c129d798c248d6e7e1c776caa1a
yanggqm <289656703@qq.com>
2018-09-02 23:23:44 +0800

    :art: commit a extension of vscode

--- extension.js
+++ extension.js
@@ -6,15 +6,20 @@ const vscode = require('vscode');
 // 在插件被激活的时候，这个方法会被调用 
 function activate(context) {
 
+    const config = {}; // 默认项
+    const params = {
+        vscode,
+        ...config
+    }
+
     // 执行 HeaderRemark 这个类
-    const headerRemark = new HeaderRemark(vscode);
+    const headerRemark = new HeaderRemark(params);
     // 需要释放的资源都在这里依次push到这个数组里面
     context.subscriptions.push(headerRemark);
 }
 
 // 对外暴露激活钩子函数
 exports.activate = activate;
-
 
 
 // 当插件失效的时候调用此方法 
--- src/HeaderRemark.js
+++ src/HeaderRemark.js
@@ -1,17 +1,27 @@
-
+/**	 
+ * @file: fileheaderremark插件核心处理逻辑类
+ * @author: yangguoqiang
+ * @param: 
+ *     props    {Object}     配置项
+ * @return: void
+ * @example: new HeaderRemark({...})
+ */
 
 // 创建头备注类
 class HeaderRemark {
 
     // 传入配置参数
     constructor(props) {
-        this.vscode = props.vscode; // 接受传过来的vscode对象
+        const {vscode} = props;
+        this.vscode = vscode; // 接受传过来的vscode对象
         this.init(); // 执行方法
     }
 
     // 执行入口
     init() {
-
+        const {
+            
+        } =  this.vscode;
     }
 }
 
